8a66fe70fe7b6423134318ff8992a471ce1c5099
Sam Perlmutter <sam.perlmutter227@gmail.com>
2015-10-08 00:14:58 -0400

    - Made the success popup appear when it's supposed to - Added a little padding to the submit button

--- js/app.js
+++ js/app.js
@@ -100,6 +100,7 @@ app.controller('FormController', function ($rootScope, $scope, $http) {
                 console.log(response);
                 $('#scouting_form').trigger('reset');
                 $('body').scrollTop(0);
+                $("#scouting_form").before('<div id="success_message" class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"<span aria-hidden="true">&times;</span></button><strong>Success!</strong> Now do it again.</div>');
             }, function (response) {
                 console.log("Error during submission");
                 console.log(response);
