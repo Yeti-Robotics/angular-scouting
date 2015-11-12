c59d9ef12bf8b0672622a18c977119e3fd8e217b
wazateer1 <wazateer1@gmail.com>
2015-11-12 12:45:25 -0500

    - Update getByteCoins.php

--- php/getByteCoins.php
+++ php/getByteCoins.php
@@ -3,8 +3,8 @@
     include("functions.php");
 
     $params = json_decode(file_get_contents('php://input'), true);
-    if(getName($db, $params["id"], md5($params["pswd"]))) {
-        getByteCoins($db, $params["id"], md5($params["pswd"]));
+    if(getName($db, $params["username"], md5($params["pswd"]))) {
+        getByteCoins($db, $params["username"], md5($params["pswd"]));
     }
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 SQL Error', true, 500);
 ?>
