61cfaaadbbc41c595446ad399432038810eb2e80
wazateer1 <wazateer1@gmail.com>
2015-11-12 10:47:27 -0500

    - added bottom-stack hashing
    Before a functions.php function can be called with the password (at the bottom of the stack), it now needs to be hashed. Im adding that

--- php/getByteCoins.php
+++ php/getByteCoins.php
@@ -3,8 +3,8 @@
     include("functions.php");
 
     $params = json_decode(file_get_contents('php://input'), true);
-    if((!empty($params["id"])) && (!empty($params["pswd"]))) {
-        getByteCoins($db, $params["id"], $params["pswd"]);
+    if(getName($db, $params["id"], md5($params["pswd"]))) {
+        getByteCoins($db, $params["id"], md5($params["pswd"]));
     }
     header($_SERVER['SERVER_PROTOCOL'] . ' 500 SQL Error', true, 500);
 ?>
