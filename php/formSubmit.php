7e013dd986445c716e7e242fbe6d7bbff2b26a77
wazateer1 <wazateer1@gmail.com>
2016-01-28 19:12:34 -0500

    changed formSubmit.php to recieve the data properly

--- php/formSubmit.php
+++ php/formSubmit.php
@@ -3,66 +3,37 @@ include("connect.php");
 include("functions.php");
 header('Content-Type: application/json');
 $postData = json_decode(file_get_contents("php://input"), true);
-$query = "INSERT INTO scout_data (team, match_number,
-         robot_moved, totes_auto, cans_auto, coopertition,
-         coopertition_totes, score, comments, rating, name,
-        cans_from_middle, totes_from_landfill, totes_from_human, cans_auto_origin, in_auto_zone)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$query = "INSERT INTO scout_data (name, match_number,
+         team, robot_moved, auto_balls_crossed, auto_balls_high,
+         auto_balls_low, teleop_balls_high, teleop_balls_low, robot_defended, rating, score, comments)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+if($postData["robot_moved"]) {
+	$robot_moved = 1;
+} else {
+	$robot_moved = 0;
+}
+if($postData["robot_defended"]) {
+	$robot_defended = 1;
+} else {
+	$robot_defended = 0;
+}
 
 if($stmt = $db->prepare($query)) {
-    $robot_moved = 0;
-    if(isset($postData["robot_moved"])){
-        $robot_moved = 1;
-    } else{
-        $robot_moved = 0;
-    }
-    $cans_from_middle = 0;
-    if(isset($postData["cans_from_middle"])){
-        $cans_from_middle = 1;
-    } else{
-        $cans_from_middle = 0;
-    }
-    $totes_from_landfill = 0;
-    if(isset($postData["totes_from_landfill"])){
-        $totes_from_landfill = 1;
-    } else{
-        $totes_from_landfill = 0;
-    }
-    $totes_from_human = 0;
-    if(isset($postData["totes_from_human"])){
-        $totes_from_human = 1;
-    } else{
-        $totes_from_human = 0;
-    }
-    $coopertition_number = 0;
-    if(isset($postData["coopertition_number"])){
-        $coopertition_number = 1;
-    } else{
-        $coopertition_number = 0;
-    }
-    $in_auto_zone = 0;
-    if (isset($postData["in_auto_zone"])) {
-        $in_auto_zone = 1;
-    } else {
-        $in_auto_zone = 0;
-    }
-
-    $stmt->bind_param("iiiiiiiisisiiiii",
-        $postData["team_number"],
-        $postData["match_number"],
-        $robot_moved,
-        $postData["totes_auto"],
-        $postData["cans_auto"],
-        $coopertition_number,
-        $postData["coopertition_totes"],
-        $postData["score"],
-        $postData["comments"],
-        $postData["rating"],
+    $stmt->bind_param("siiiiiiiiiiis",
         $postData["name"],
-        $cans_from_middle,
-        $totes_from_landfill,
-        $totes_from_human,
-        $postData["cans_auto_origin"],
+        $postData["match_number"],
+		$postData["team"],
+        $robot_moved,
+        $postData["auto_balls_crossed"],
+        $postData["auto_balls_high"],
+        $postData["auto_balls_low"],
+        $postData["teleop_balls_high"],
+        $postData["teleop_balls_low"],
+        $robot_defended,
+        $postData["rating"],
+		$postData["score"],
+        $postData["comments"],
         $in_auto_zone);
     $stmt->execute();
     if ($stmt->error) {
@@ -71,24 +42,13 @@ if($stmt = $db->prepare($query)) {
 	    die('{"message":"'.$stmt->error.'"}');
     }
     $insert_id = $stmt->insert_id;
-    if(count($postData["stackRows"]["rows"]) > 0) {
-        foreach ($postData["stackRows"]["rows"] as $stack) {
-            $stack_query = "INSERT INTO stacks (scout_data_id, totes, cap_state, cap_height)
-                    VALUES (?, ?, ?, ?)";
-            if ($stack_stmt = $db->prepare($stack_query)) {
-                $stack_stmt->bind_param("iiii", $insert_id,  intval($stack["stacks_totes"]), intval($stack["capped_stack"]), intval($stack["cap_height"]));
-                $stack_stmt->execute();
-//                echo("Stack height: ".$stack["stacks_totes"].", Cap state: ".$stack["capped_stack"].", Cap height: ".$stack["cap_height"]."\n\n");
-            }
-        }
-    }
 } else {
     header('HTTP/1.1 500 SQL Error', true, 500);
     $db->close();
 	die ( '{"message":"Failed creating statement"}' );
 }
 
-updateTeamInfo($db, $postData["team_number"]);
+updateTeamInfo($db, $postData["team"]);
 
 $db->close();
 ?>
