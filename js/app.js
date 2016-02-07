a2bbc8ba79c37c9794cdbf8eb52d58bd72d1a06e
Sam Perlmutter <sam.perlmutter227@gmail.com>
2016-02-07 01:02:03 -0500

    - Fixed form data not resetting

--- js/app.js
+++ js/app.js
@@ -168,49 +168,51 @@ app.controller('RegisterController', function ($scope, $http, $location) {
 app.controller('FormController', function ($rootScope, $scope, $http, $window) {
 	'use strict';
 
-	$scope.templateFormData = {
-		name: $rootScope.user.name,
-		robot_moved: false,
-		auto_defense_crossed: {
-            portcullis: 0,
-            cheval_de_frise: 0,
-            moat: 0,
-            ramparts: 0,
-            drawbridge: 0,
-            sally_port: 0,
-            rock_wall: 0,
-            rough_terrain: 0,
-            low_bar: 0
-        },
-		auto_balls_crossed: 0,
-		auto_balls_scored: [],
-		auto_balls_high: 0,
-		auto_balls_low: 0,
-		teleop_defense_crossed: {
-            portcullis: 0,
-            cheval_de_frise: 0,
-            moat: 0,
-            ramparts: 0,
-            drawbridge: 0,
-            sally_port: 0,
-            rock_wall: 0,
-            rough_terrain: 0,
-            low_bar: 0
-        },
-		teleop_balls_scored: [],
-		teleop_balls_high: 0,
-		teleop_balls_low: 0,
-		robot_defended: false,
-		end_game: "none",
-		rating: 1,
-		score: 0,
-		comments: ""
-	};
-	$scope.formData = $scope.templateFormData;
+    $scope.resetForm = function() {
+        $scope.formData = {
+            name: $rootScope.user.name,
+            robot_moved: false,
+            auto_defense_crossed: {
+                portcullis: 0,
+                cheval_de_frise: 0,
+                moat: 0,
+                ramparts: 0,
+                drawbridge: 0,
+                sally_port: 0,
+                rock_wall: 0,
+                rough_terrain: 0,
+                low_bar: 0
+            },
+            auto_balls_crossed: 0,
+            auto_balls_scored: [],
+            auto_balls_high: 0,
+            auto_balls_low: 0,
+            teleop_defense_crossed: {
+                portcullis: 0,
+                cheval_de_frise: 0,
+                moat: 0,
+                ramparts: 0,
+                drawbridge: 0,
+                sally_port: 0,
+                rock_wall: 0,
+                rough_terrain: 0,
+                low_bar: 0
+            },
+            teleop_balls_scored: [],
+            teleop_balls_high: 0,
+            teleop_balls_low: 0,
+            robot_defended: false,
+            end_game: "none",
+            rating: 1,
+            score: 0,
+            comments: ""
+        };
+    };
 
 	$(document).ready(function () {
 		$('#scouting_form').validate();
 		console.log('Inititalize validation');
+        $scope.resetForm();
 	});
 
 	$scope.submit = function () {
@@ -243,7 +245,7 @@ app.controller('FormController', function ($rootScope, $scope, $http, $window) {
 				if ($('#scouting_form').prev().attr('id') != "success_message") {
 					$("#scouting_form").before('<div id="success_message" class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"<span aria-hidden="true">&times;</span></button><strong>Success!</strong> Now do it again.</div>');
 				}
-				$scope.formData = $scope.templateFormData;
+				$scope.resetForm();
 			}, function (response) {
 				console.log("Error during submission");
 				console.log(response);
