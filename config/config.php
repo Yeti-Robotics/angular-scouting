0481ea3832beaf694114fd394bd0f93999fb347a
FlamingSpork <philo_bunny@yahoo.com>
2016-04-25 20:15:30 -0400

    Readded config.php

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+// database connection variables
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpassword = "root";
+$dbname = "scouting";
+
+// FMSAPI variables
+$tournamentKey = "NYRO";
+$tournamentYear = 2016;
+$authUser = "user";
+$authToken = "token";
+$apiServer = "https://frc-api.firstinspires.org/v2.0";
+
+// Admin Account details
+$adminUsername = "admin";
+$adminPswdHash = "5f4dcc3b5aa765d61d8327deb882cf99"; // It's "password"
+?>
