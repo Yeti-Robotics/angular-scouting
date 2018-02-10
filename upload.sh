4380f04c5012671214c382fd03def6047dfe4cef
Antoine Campbell <acampbell@captechventures.com>
2018-02-10 15:53:45 -0500

    - Added upload script

---
+++ upload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+read -p "Enter user with domain, e.g. user@example.com: " USERNAME
+
+# Remove exisiting archive
+rm archive.zip
+
+# Zip directory
+ zip -x ".git/*" -x ".vscode/*" -x "config/*" -r archive.zip ./
+
+ # Upload zip to scouting
+scp archive.zip ${USERNAME}:/home/wampa3506/scouting
+
+# Unzip file archive
+ssh ${USERNAME} << EOF
+cd /home/wampa3506/scouting
+unzip -o archive.zip
+rm archive.zip
